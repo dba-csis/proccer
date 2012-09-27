5a4b810b69976727fedc9c68040520a53bb358ee
Sune Kirkeby <ski@csis.dk>
2012-09-27 08:54:26 +0000

    New version 0.7.8 with actual, working setup.py.

--- setup.py
+++ setup.py
@@ -4,15 +4,26 @@ from setuptools import setup, find_packages
 
 setup(
     name='proccer',
-    version='0.7.7',
+    version='0.7.8',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=True,
-    install_requires=filter(None, [
-        line.split('#')[0].strip()
-        for line in open('requirements.txt')
-    ]),
+    install_requires='''
+        PyYAML==3.09
+        SQLAlchemy==0.7.6
+        Werkzeug==0.8.1
+        Flask==0.8
+        Flask-Genshi==0.5.1
+        blinker==1.2
+        jsonlib==1.3.10
+        lockfile==0.8
+        mock==0.8.0
+        psycopg2==2.4.5
+        requests>=0.6.1
+        wsgi-lite==0.5a2
+        wsgiref==0.1.2
+    ''',
     entry_points='''
         [console_scripts]
         proccer = proccer.console_scripts:run_processes
