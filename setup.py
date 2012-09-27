9cc0fdbf73ce13aef1c213789ca09900b1464b37
Sune Kirkeby <ski@csis.dk>
2012-09-27 11:36:37 +0000

    Fix manifest.
    I want a hit of whatever the distribute guys are smoking.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='proccer',
-    version='0.7.10',
+    version='0.7.11',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
