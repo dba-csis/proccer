81a0c01de0c3461b49088ca57e0eb555e04e7d03
Sergio Oliveira <sro@csis.dk>
2013-10-21 11:03:27 +0200

    Test human readable memory limit specification

--- src/proccer/t/test_agent.py
+++ src/proccer/t/test_agent.py
@@ -1,14 +1,20 @@
 from __future__ import with_statement
 
+from math import pow
 from mock import patch, call
 from nose.tools import eq_
 import os
 import resource
 from tempfile import NamedTemporaryFile
 
-from proccer.agent import read_configuration, report, set_memory_limit, \
-    default_memory_limit, _in_parent, _in_child
-from proccer.t.testing import assert_eq
+from proccer.agent import (read_configuration,
+                           report,
+                           memory_size_human_to_bytes,
+                           set_memory_limit,
+                           default_memory_limit,
+                           _in_parent,
+                           _in_child)
+from proccer.t.testing import assert_eq, assert_raises
 
 
 def test_report():
@@ -66,10 +72,77 @@ def test_with_overrides():
     assert parsed == expected, parsed
 
 
+def test_memory_conversion():
+    eq_(memory_size_human_to_bytes('1'), 1)
+    eq_(memory_size_human_to_bytes('1k'), 1024)
+    eq_(memory_size_human_to_bytes('1K'), 1024)
+    eq_(memory_size_human_to_bytes('1 k'), 1024)
+    eq_(memory_size_human_to_bytes('1 K'), 1024)
+    eq_(memory_size_human_to_bytes('1kb'), 1024)
+    eq_(memory_size_human_to_bytes('1KB'), 1024)
+    eq_(memory_size_human_to_bytes('1 kb'), 1024)
+    eq_(memory_size_human_to_bytes('1 KB'), 1024)
+    eq_(memory_size_human_to_bytes('1  k'), 1024)
+    eq_(memory_size_human_to_bytes('1M'), pow(1024, 2))
+    eq_(memory_size_human_to_bytes('1MB'), pow(1024, 2))
+    eq_(memory_size_human_to_bytes('1G'), pow(1024, 3))
+    eq_(memory_size_human_to_bytes('1GB'), pow(1024, 3))
+    eq_(memory_size_human_to_bytes('1T'), pow(1024, 4))
+    eq_(memory_size_human_to_bytes('1TB'), pow(1024, 4))
+    eq_(memory_size_human_to_bytes('1P'), pow(1024, 5))
+    eq_(memory_size_human_to_bytes('1PB'), pow(1024, 5))
+    eq_(memory_size_human_to_bytes('1E'), pow(1024, 6))
+    eq_(memory_size_human_to_bytes('1EB'), pow(1024, 6))
+    eq_(memory_size_human_to_bytes('1Z'), pow(1024, 7))
+    eq_(memory_size_human_to_bytes('1ZB'), pow(1024, 7))
+    eq_(memory_size_human_to_bytes('1Y'), pow(1024, 8))
+    eq_(memory_size_human_to_bytes('1YB'), pow(1024, 8))
+    eq_(memory_size_human_to_bytes('2.5 GB'), int(2.5 * pow(1024, 3)))
+
+
+def test_memory_read_conf_int():
+    yaml = '''
+        commands:
+            t: {command: "true", memory-limit: 50000000}
+    '''
+    parsed = read_configuration(yaml)
+    expected = {
+        'commands': {
+            't': {
+                'command': 'true',
+                'memory-limit': 50000000,
+            }
+        }
+    }
+    eq_(parsed, expected)
+
+
+def test_memory_read_conf_human():
+    yaml = '''
+        commands:
+            t: {command: "true", memory-limit: '50M'}
+    '''
+    parsed = read_configuration(yaml)
+    expected = {
+        'commands': {
+            't': {
+                'command': 'true',
+                'memory-limit': '50M',
+            }
+        }
+    }
+    eq_(parsed, expected)
+
+
 def check_memvalue(job_desc):
     '''Check if the correct memory limit is applied'''
 
-    expected_memory_value = job_desc.get('memory-limit', default_memory_limit)
+    specified_memory_value = job_desc.get('memory-limit', default_memory_limit)
+    if isinstance(specified_memory_value, int):
+        expected_memory_value = specified_memory_value
+    else:
+        expected_memory_value = \
+            memory_size_human_to_bytes(specified_memory_value)
 
     with patch('resource.setrlimit') as resource_setrlimit:
         set_memory_limit(job_desc)
@@ -89,6 +162,47 @@ def test_memvalue_generator():
         'memory-limit': 50000000
     }
     yield check_memvalue, limited_job_desc
+
+    human_readable_job_desc = {
+        'command': 'true',
+        'memory-limit': '50M'
+    }
+    yield check_memvalue, human_readable_job_desc
+
+
+def check_memory_invalid(invalid_job_desc):
+    with patch('resource.setrlimit'):
+        with assert_raises(Exception):
+            set_memory_limit(invalid_job_desc)
+
+
+def test_memory_invalid():
+    '''Tests if the default memory limit is applyed if the specified value
+    is invalid'''
+
+    invalid_unit_job = {
+        'command': 'true',
+        'memory-limit': '666 zillion'
+    }
+    yield check_memory_invalid, invalid_unit_job
+
+    invalid_value_job = {
+        'command': 'true',
+        'memory-limit': '6.6.6 MB'
+    }
+    yield check_memory_invalid, invalid_value_job
+
+    empty_field_job = {
+        'commad': 'true',
+        'memory-limit': ''
+    }
+    yield check_memory_invalid, empty_field_job
+
+    invalid_format_job = {
+        'commad': 'true',
+        'memory-limit': '1m2m3m4m'
+    }
+    yield check_memory_invalid, invalid_format_job
 
 
 def fork_and_wait(name, desc):
