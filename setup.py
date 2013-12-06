2ddc4db1412625882b938fc1904fede1aa14585d
Sune Kirkeby <ski@csis.dk>
2013-12-06 12:37:29 +0100

    Use non-broken lockfile version.

--- setup.py
+++ setup.py
@@ -4,17 +4,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='proccer',
-    version='0.7.22',
+    version='0.7.23',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
     # These requirements are for the agent. To install the manager's
     # requirements also do `pip install -r manager-requirements.txt`
+    # Note: We force lockfile to version 0.8, since that version is known to
+    # work (0.9.1 is broken wrt. timeout=0 during acquire).
     install_requires='''
         PyYAML
         jsonlib
-        lockfile
+        lockfile == 0.8
         requests
     ''',
     entry_points='''
